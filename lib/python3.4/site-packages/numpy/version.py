07aeabdcf5715265a64ab7262bd8e863aefa4713
JJHedges <jordanjameshedges3@gmail.com>
2015-08-28 19:22:29 +0800

    Testing non-stl

---
+++ lib/python3.4/site-packages/numpy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM NUMPY SETUP.PY
+short_version = '1.10.0b1'
+version = '1.10.0b1'
+full_version = '1.10.0b1'
+git_revision = 'b9640613ddeaa5993916718ebec8c71d4001d489'
+release = True
+
+if not release:
+    version = full_version
