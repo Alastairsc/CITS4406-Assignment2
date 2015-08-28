ec9b114e44243b14ffc785d5b9744abe833ff381
JJHedges <jordanjameshedges3@gmail.com>
2015-08-28 18:29:37 +0800

    Added standard distribution testing using numpy/scipy

--- data.py
+++ data.py
@@ -6,6 +6,8 @@ import re
 from collections import Counter
 from statistics import mean, mode, median_low, median, median_high, stdev, \
     StatisticsError, Decimal
+from scipy.stats import mstats
+import numpy as np
 
 
 #  Config
@@ -60,13 +62,18 @@ class CurrencyAnalyser(Analyser):
     def __init__(self, values):
         values = [eval(i) for i in values]
         super().__init__(values)
+        self.pval = mstats.normaltest(np.array(values))[1]
+        #print(self.pval)
         self.min = min(values)
         self.max = max(values)
         self.mean = Decimal(mean(values)).quantize(Decimal('.00'))
         self.median_low = median_low(values)
         self.median = median(values)
         self.median_high = median_high(values)
-        self.stdev = Decimal(stdev(values)).quantize(Decimal('.00'))
+        if(self.pval < 0.055):
+            self.stdev = Decimal(stdev(values)).quantize(Decimal('.00'))
+        else:
+            self.stdev = 'N/A'
 
 class StringAnalyser(Analyser):
     """Run string analysis."""
@@ -87,13 +94,17 @@ class NumericalAnalyser(Analyser):
     def __init__(self, values):
         values = [eval(i) for i in values]
         super().__init__(values)
+        self.pval = mstats.normaltest(np.array(values))[1]
         self.min = min(values)
         self.max = max(values)
         self.mean = Decimal(mean(values)).quantize(Decimal('.00000'))
         self.median_low = median_low(values)
         self.median = median(values)
         self.median_high = median_high(values)
-        self.stdev = Decimal(stdev(values)).quantize(Decimal('.00000'))
+        if(self.pval < 0.055):
+            self.stdev = Decimal(stdev(values)).quantize(Decimal('.00'))
+        else:
+            self.stdev = 'N/A'
 
 class BooleanAnalyser(Analyser):
     "Run email analysis"
