9b508e5f960e8e8bf99f5257fd6384000a08f7b3
JJHedges <jordanjameshedges3@gmail.com>
2015-09-07 20:53:01 +0800

    Added wrong value detection and added output to report

--- report.py
+++ report.py
@@ -50,6 +50,8 @@ and create required local variables.
             invalid_rows=self.list_creator(self.data.invalid_rows), 
             empty_columns=self.list_creator(self.empty_columns()),
             len_empty_columns=len(self.empty_columns()),
+            error_columns=self.list_creator(self.data.errors),
+            len_error_columns=len(self.data.errors),
             len_columns=len(self.data.valid_rows),
             numerical_analysis=self.numerical_analysis(),
             string_analysis=self.string_analysis(),
