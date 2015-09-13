b1a5b1e761450697787df04540c01861bb63be40
Alastairsc <gassyhue@hotmail.com>
2015-09-13 14:38:16 +0800

    Added command line options for use with multiple files and a single template

--- application.py
+++ application.py
@@ -2,42 +2,40 @@
 """
 
 import sys
+import argparse
 
 from data import *
 from report import *
 from editor import *
 
-def main(filePath):
+def main(*args):
     """Create Data and Report objects, providing necessary information for them 
     to run analysis and create desired outputs (i.e. HTML report).
 
     """
-    file = filePath
-    data = Data(file)
+    file = args[0]
+    if len(args) > 1:
+        temp = Template(args[1])
+        data = Data(file, temp)
+    else:
+        data = Data(file)
     data.clean()
   #  editor = Editor(data)
     data.analyse()
   #  editor.make_corrected(file)
     report = Report(data, file)
     report.html_report()
-
-def mainT(filePath, template):
-    """"Creates Data and Report objects using the template as a guideline"""
-    
-    temp = Template(template)
-    file = filePath
-    data = Data(file, temp)
-    data.clean()
-    data.analyse()
-    report = Report(data, file)
-    report.html_report()
-    
-if __name__ == '__main__':
-    numFiles = len(sys.argv)
-    #for i in range(1,numFiles):
-    #    main(sys.argv[i])
-    if numFiles > 2:
-        mainT(sys.argv[1], sys.argv[2])
+            
+if __name__ == '__main__': 
+    parser = argparse.ArgumentParser(description='Processes Csv files.')
+    parser.add_argument('filenames', nargs='+',\
+        help='one or more filenames for the processor to analyse')
+    parser.add_argument('-t', metavar='template', default='',help='a template for the given files')
+    args = parser.parse_args()
+    print (args)
+    if args.t != '':
+        for name in args.filenames:
+            main(name, args.t)
     else:
-        for i in range(1,numFiles):
-            main(sys.argv[i])
+        for name in args.filenames:
+            main(name)
