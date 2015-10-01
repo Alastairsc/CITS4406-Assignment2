f9fe5b8f48ee2c776359df06659296b16b3c0061
Alastairsc <gassyhue@hotmail.com>
2015-10-01 14:26:53 +0800

    Moved templates to own folder

---
+++ test.py
@@ -0,0 +1,8 @@
+f_in = open('C:\\Users\\Alastair\\Dropbox\\DataAnalysisProject\\FromMelinda\\GFunearthedallocnew.csv', 'rU')
+f_out = open("csv_files\largefile.csv", 'w')
+f_out.write(f_in.readline())
+for i in range(0, 5):
+    f_in.seek(0)
+    f_in.readline()
+    for line in f_in:
+        f_out.write(line)
