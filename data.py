3162a8ba0456f9bff12e9f3aac934304654d727d
Django User <djangouser@CITS3200-Ubuntu.UWADataAnalysis.q8.internal.cloudapp.net>
2015-10-10 07:58:57 +0000

    fixed encoding issue

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# -*- coding: iso-8859-15 -*-
 """Reads CSV file for information, provides basic cleaning of data and then
 runs analysis on said data."""
 
